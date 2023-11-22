6b0001b5831a6aeb92f165500034c880a425013f
dlam <phena109@gmail.com>
2023-11-23 04:01:53 +1100

    area-city-map api endpoint

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,8 @@
 <?php
 
+use App\ATDW\Models\Region;
+use App\ATDW\Services\Products;
+use App\ATDW\State;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -17,3 +20,8 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+
+Route::get('/area-city-map', static fn(): array => (new Products())->getAllAreasSuburbsByRegion(
+    Region::fromStateAndArray(State::NSW, config('params.Greater Sydney Region'))
+));
