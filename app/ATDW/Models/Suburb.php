e2133f05ed83bdf7d515f6a3d1a574c591098bec
dlam <phena109@gmail.com>
2023-11-23 03:48:18 +1100

    remove unneeded setters

--- app/ATDW/Models/Suburb.php
+++ app/ATDW/Models/Suburb.php
@@ -21,7 +21,7 @@ class Suburb
     {
         $properties = array_intersect_key($data, static::NAME_MAP);
         $output = new static();
-        $output->setState($state);
+        $output->state = $state;
         foreach ($properties as $property => $value) {
             $propertyName = static::NAME_MAP[$property];
             if (property_exists(static::class, $propertyName)) {
@@ -46,38 +46,22 @@ class Suburb
         return $this->suburbId;
     }
 
-    public function setSuburbId(string $suburbId): void
-    {
-        $this->suburbId = $suburbId;
-    }
 
     public function getName(): string
     {
         return $this->name;
     }
 
-    public function setName(string $name): void
-    {
-        $this->name = $name;
-    }
 
     public function getPostCode(): string
     {
         return $this->postCode;
     }
 
-    public function setPostCode(string $postCode): void
-    {
-        $this->postCode = $postCode;
-    }
 
     public function getState(): State
     {
         return $this->state;
     }
 
-    public function setState(State $state): void
-    {
-        $this->state = $state;
-    }
 }
