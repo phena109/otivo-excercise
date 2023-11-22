859854536ff07483e794f61aa270aaa3ab5e5840
dlam <phena109@gmail.com>
2023-11-22 12:45:45 +1100

    1 new test

--- tests/Unit/App/ATDW/Services/ApiCallTest.php
+++ tests/Unit/App/ATDW/Services/ApiCallTest.php
@@ -3,6 +3,8 @@
 namespace Unit\App\ATDW\Services;
 
 use App\ATDW\Services\ApiCall;
+use App\ATDW\Utf16LeParser;
+use GuzzleHttp\Exception\GuzzleException;
 use PHPUnit\Framework\TestCase;
 
 class ApiCallTest extends TestCase
@@ -28,5 +30,21 @@ class ApiCallTest extends TestCase
         $apiCall = new ApiCall('foo', 'bar');
         $url = $apiCall->getGetUrl(['foobar' => '2023']);
         static::assertSame('https://atlas.atdw-online.com.au/api/atlas/foo?key=bar&foobar=2023', $url);
+    }
+
+    public function test_get(): void
+    {
+        $this->markTestSkipped('Manually enable if needed');
+        $apiCall = new ApiCall('areas', '123456789101112');
+        try {
+            $result = $apiCall->get(['out' => 'json']);
+        } catch (GuzzleException $e) {
+            static::fail($e->getMessage());
+        }
+        $body = new Utf16LeParser($result->getBody());
+        $array = $body->getArray();
+        static::assertNotNull($array);
+        static::assertIsArray($array);
+        static::assertGreaterThan(0, count($array));
     }
 }
